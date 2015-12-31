f0b8a5f24231bda470e2a803e51e7ce024b50a8c
PHOTU <officialphotu@gmail.com>
2015-12-31 20:41:06 +0000

    Signed-off-by: PHOTU <officialphotu@gmail.com>

--- categories/general/personal-success.php
+++ categories/general/personal-success.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-        <title>PHOTU - 2016 Changes</title>
-        <meta name="description" content="We take a look through our planned changes for 2016.">
+        <title>PHOTU - Personal Success</title>
+        <meta name="description" content="How do you judge personal success?">
 
     <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/loadinstyles.php' ); ?>
 
@@ -27,7 +27,7 @@
             &nbsp;|&nbsp;
             <span itemtype="http://data-vocabulary.org/Breadcrumb" itemscope="">
                 <a itemprop="url" class="current-page">
-                    <span itemprop="title">2016 Changes</span>
+                    <span itemprop="title">Personal Success</span>
                 </a>
             </span>
         </div>
@@ -47,34 +47,19 @@
                         <div class="pd-20 kopa-entry-post">
                             <article class="entry-item">
                                 <p class="entry-categories style-s"><a href="/categories/general">General</a></p>
-                                <h4 class="entry-title">2016 is set to be a great year with huge overhauls for PHOTU on the horizon.</h4>
+                                <h4 class="entry-title">How do you judge your own personal success and how it defines you?</h4>
                                 <div class="entry-meta mb-20">
                                     <a href="#">
                                     <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/entries/author-adam.php' ); ?> 
                                     </a>
                                     &nbsp;|&nbsp;
-                                    <span class="entry-date"><i class="fa fa-clock-o"></i>Dec 20, 2015</span>
+                                    <span class="entry-date"><i class="fa fa-clock-o"></i>Jan 1, 2016</span>
                                 </div>
                                 <div class="entry-thumb">
-                                    <img src="/images/categories/general/2016-changes/2016-changes.jpg" alt="">
+                                    <img src="/images/categories/general/personal-success/personal-success.png" alt="">
                                 </div>  
-                                <p>You can expect.. </p>
-                                
-
-                        <div class="clearfix">
-                            <h4 class="element-title">2016 Changes</h4>
-                            <div class="col-md-12 col-sm-12 element-wrap e-list">
-                                <div class="">   
-                                    <div class="col-md-12 col-sm-12 col-xs-12">
-                                        <ul class="kopa-e-list">
-                                            <li><span class="fa fa-check"></span>CSS Updates including new flexible content areas</li>
-                                            <li><span class="fa fa-check"></span>Content submission area</li>
-                                            <li><span class="fa fa-check"></span>Comments section for each entry</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                                <p>..</p>
+                            
 
                         <p>
                             
