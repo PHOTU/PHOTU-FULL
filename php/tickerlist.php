eb6a407beb223af62ee07ce3f2ba990d730dfe2d
PHOTU <officialphotu@gmail.com>
2015-12-31 17:10:14 +0000

    Signed-off-by: PHOTU <officialphotu@gmail.com>

--- php/tickerlist.php
+++ php/tickerlist.php
@@ -1,8 +1,6 @@
 <div class="kopa-ticker">
                         <h6 class="ticker-title">Team Message</h6>
                         <ul id="js-news" class="news-white-text">
-                            <li>Merry Christmas to all our users, have a great time - We'll see you in 2016!</li>
-                            <li>Got something techy for Christmas? Contact us and let us know!</li>
-                        	<li>We wish all our users a very happy new year, good luck for 2016!</li>
+                            <li>Welcome to 2016! New changes to come very soon!</li>
                         </ul>
                     </div>
