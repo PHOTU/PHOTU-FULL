ac199fb6449d7996df58984526a6334caa0bc918
PHOTU <officialphotu@gmail.com>
2016-01-02 19:04:15 +0000

    Signed-off-by: PHOTU <officialphotu@gmail.com>

--- php/index-categories/music.php
+++ php/index-categories/music.php
@@ -1,7 +1,7 @@
 <div class="item">
                                 <article class="entry-item">
                                     <div class="entry-thumb">
-                                        <a href="/categories/technology"><img src="/images/index/categories/music.png" alt=""></a>
+                                        <a href="/categories/music"><img src="/images/index/categories/music.png" alt=""></a>
                                     </div>
                                 </article>
                             </div>
