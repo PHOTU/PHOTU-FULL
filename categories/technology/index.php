d26bffcf87675dc1cfcc8bd5e1207b41c64c84eb
PHOTU <officialphotu@gmail.com>
2016-01-08 21:02:37 +0000

    Category Image

--- categories/technology/index.php
+++ categories/technology/index.php
@@ -4,34 +4,20 @@
         <meta name="description" content="Browse our Technology Entries">
 
 <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/loadinstyles.php' ); ?>
-    
+
 <body class="">
 
 <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/header.php' ); ?>
 
-    <div class="kopa-breadcrumb">
-        <div class="wrapper clearfix">
-            <span itemtype="http://data-vocabulary.org/Breadcrumb" itemscope="">
-                <a itemprop="url" href="/">
-                    <span itemprop="title">Home</span>
-                </a>
-            </span>
-            &nbsp;|&nbsp;
-            <span itemtype="http://data-vocabulary.org/Breadcrumb" itemscope="">
-                <a itemprop="url" class="current-page">
-                    <span itemprop="title">Technology</span>
-                </a>
-            </span>
-        </div>
-    </div>
-    <!--/end .breadcrumb-->
 
     <div id="main-content" class="mb-20">
 
         <div class="wrapper">
 
+          <img src="/images/categories/technology/technology.jpg">
+
             <div class="row">
-            
+
                 <div class="kopa-main-col">
 
                     <div class="widget kopa-masonry-6-widget">
@@ -40,7 +26,7 @@
 
                     <!-- ADD ENTRIES HERE -->
                             <?php include( $_SERVER['DOCUMENT_ROOT'] . '/categories/technology/entry-list/matty-gale-youtube.php' ); ?>
-                        
+
                     <!-- END OF ENTRIES LIST -->
 
 
@@ -59,7 +45,7 @@
                     </div>
                     <!-- widget -->
                 <!-- END PAGINATION -->
-            
+
                 </div>
                 <!-- kopa-main-col -->
 
@@ -74,14 +60,14 @@
 
                 <!-- SHOW ANY EDITORS PICKS? -->
                 <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/categories/editor-picks/reviews.php' ); ?>
-                   
-                
+
+
                 </div>
                 <!-- kopa-col-2 -->
 
             </div>
-            <!-- row --> 
-        
+            <!-- row -->
+
         </div>
         <!-- wrapper -->
 
@@ -90,7 +76,7 @@
 
 
 <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/footer.php' ); ?>
-    
+
 </body>
 
 </html>
