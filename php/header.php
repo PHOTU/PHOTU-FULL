71fc9ef713fdf7ef7b5c2b390edfb7f3acd1903f
PHOTU <officialphotu@gmail.com>
2016-01-16 10:41:41 +0000

    k

---
+++ php/header.php
@@ -0,0 +1,98 @@
+<!-- Add Analytics.. -->
+<script src="/php/analyticstracking.php"></script>
+
+<header class="kopa-header">
+
+        <div class="kopa-header-top">
+
+            <div class="wrapper">
+
+                <div class="header-top-left">
+
+                    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/tickerlis.php' ); ?>
+
+                </div>
+                <!-- header-top-left -->
+
+            </div>
+            <!-- wrapper -->
+
+        </div>
+        <!-- kopa-header-top -->
+
+        <div class="kopa-header-middle">
+
+            <div class="wrapper">
+
+                <div class="kopa-logo">
+                    <a href="/"><img src="/images/logo.png" alt="logo"></a>
+                </div>
+                <!-- logo -->
+
+                <div class="kopa-social-links pull-right">
+                    <ul class="clearfix">
+                        <li><a target="_blank" href="http://www.facebook.com/officialphotu" class="fa fa-facebook"></a></li>
+                        <li><a href="#" class="fa fa-twitter"></a></li>
+                    </ul>
+                </div>
+                <!-- kopa-social-links -->
+
+            </div>
+            <!-- wrapper -->
+
+        </div>
+        <!-- kopa-header-middle -->
+
+
+
+
+
+
+
+
+        <!-- MAIN DESKTOP MENU -->
+
+        <div class="kopa-header-bottom">
+
+            <div class="wrapper">
+
+                <div class="header-bottom-wrap">
+
+                    <nav class="kopa-main-nav">
+                        <ul class="main-menu sf-menu">
+                            <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/menulist.php' ); ?>
+                        </ul>
+                    </nav>
+
+        <!-- MAIN DESKTOP MENU -->
+
+
+
+
+        <!-- MAIN MOBILE MENU -->
+
+                    <nav class="main-nav-mobile">
+                        <a class="pull">Menu<i class="fa fa-caret-down"></i></a>
+                        <ul class="main-menu-mobile">
+                            <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/menulist.php' ); ?>
+                        </ul>
+                    </nav>
+
+        <!-- MAIN MOBILE MENU -->
+
+
+
+
+
+
+                </div>
+                <!-- header-bottom-wrap -->
+
+            </div>
+            <!-- wrapper -->
+
+        </div>
+        <!-- kopa-header-bottom -->
+
+    </header>
+    <!-- kopa-page-header -->
