0e7f393f39a35e1e2f5a6867a6a0747153ee318a
PHOTU <officialphotu@gmail.com>
2016-01-17 20:00:40 +0000

    Entry Fix

--- categories/music/discover/17-01-2016.php
+++ categories/music/discover/17-01-2016.php
@@ -63,9 +63,13 @@
 
                                 <!-- Add Content -->
 
-                                <p class="entry-text">Sigh, although we all wish it was summer time it isn't unfortunately, so let's make do with some tunes to shrug off those winter blues.</p>
+                                <p class="entry-text">
+                                  Sigh, although we all wish it was summer time it isn't unfortunately, so let's make do with some tunes to shrug off those winter blues.<br>
+                                  I can't guarentee that you'll like our wide range of selections this week, but give them a try you might surprise yourself.
+                                </p>
 
-                        <div class="col-md-12 col-sm-12 col-xs-12">
+
+                        <br><div class="col-md-12 col-sm-12 col-xs-12">
 
                             <h6 class="txt-up">Pick through our selections..</h6>
                                     <div class="tab-pane active" id="e-tab1">
@@ -76,14 +80,11 @@
                                             <li><i class="order-num">#2</i>
                                               <a target="_blank" href="https://open.spotify.com/track/78g2GVsDOMef8nC1J1EDkD">Make Believers - Go Periscope</a>
                                             </li>
+                                            <li><i class="order-num">#3</i>
+                                              <a target="_blank" href="https://open.spotify.com/track/77tR4iQgyQTAnhhuPik3ZP">One For The Money - Escape The Fate</a>
+                                            </li>
 
                                         </ul>
-                                    </div>
-                                    <div class="tab-pane" id="e-tab2">
-                                        <p>.</p>
-                                    </div>
-                                    <div class="tab-pane" id="e-tab3">
-                                        <p>.</p>
                                     </div>
                                 </div>
                             </div>
