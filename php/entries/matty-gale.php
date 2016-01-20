5dfdc6b7dafb8ac45b1ec5da27a716c51b7c4d93
PHOTU <officialphotu@gmail.com>
2016-01-20 21:06:59 +0000

    Investigating

--- php/entries/matty-gale.php
+++ php/entries/matty-gale.php
@@ -1,4 +1,4 @@
                                         <div class="entry-author">
-                                            <a img href="/authors" src="/images/authors/matty-gale.png" alt=""></a>
+                                            <img src="/images/authors/admin.png" alt="">
                                             <span>Matty Gale</span>
                                         </div>
