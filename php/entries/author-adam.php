9ed7f23b2ce069cc42d0973f6b0297eeb05fe405
PHOTU <officialphotu@gmail.com>
2016-01-20 21:20:56 +0000

    Fixing Authors

--- php/entries/author-adam.php
+++ php/entries/author-adam.php
@@ -1,6 +1,6 @@
 <a href="/authors">
   <div class="entry-author">
-      <img src="/images/authors/adam-burdass.jpg" alt="">
+      <img src="/images/authors/adam-burdass-icon.jpg" alt="">
       <span>Adam Burdass</span>
   </div>
 </a>
--- php/entries/matty-gale.php
+++ php/entries/matty-gale.php
@@ -1,6 +1,6 @@
 <a href="/authors">
   <div class="entry-author">
-      <img src="/images/authors/matty-gale.png" alt="">
+      <img src="/images/authors/matty-gale-icon.jpg" alt="">
       <span>Matty Gale</span>
   </div>
 </a>
