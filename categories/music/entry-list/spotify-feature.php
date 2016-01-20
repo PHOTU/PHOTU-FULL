ca70b2fa942cebaf87e9ceae0a4bb6a6cce4f7a7
PHOTU <officialphotu@gmail.com>
2016-01-20 20:32:48 +0000

    Little tests

--- categories/music/entry-list/spotify-feature.php
+++ categories/music/entry-list/spotify-feature.php
@@ -15,17 +15,8 @@
                                                 &nbsp;|&nbsp;
                                                 <span class="entry-date"><i class="fa fa-clock-o"></i>Dec 17, 2015</span>
                                             </div>
-                                            <p>We dig into Spotify to see why it's one of most recognised music apps available. We might even add some sneaky tracks for you to enjoy.</p> 
+                                            <p>We dig into Spotify to see why it's one of most recognised music apps available. We might even add some sneaky tracks for you to enjoy.</p>
                                         </div>
-                                    </div>
-                                    <footer class="entry-footer">
-                                        <div class="kopa-social-links pull-left style4">
-                                            <ul class="clearfix">
-                                                <li><a href="#" class="fa fa-facebook"></a></li>
-                                                <li><a href="#" class="fa fa-twitter"></a></li>
-                                            </ul>
-                                        </div>
-                                        <span class="pull-right kopa-entry-view">HOT</span>
-                                    </footer>       
+                                    </div>    
                                 </article>
                             </li>
