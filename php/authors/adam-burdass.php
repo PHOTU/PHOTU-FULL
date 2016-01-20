e09c1808a6cc67d69197295d2e3e46ed52215908
PHOTU <officialphotu@gmail.com>
2016-01-20 21:39:57 +0000

    Fixed Authors Snippet

--- php/authors/adam-burdass.php
+++ php/authors/adam-burdass.php
@@ -5,12 +5,6 @@
                                     <p>Hobbyist Developer, Technology Enthusiast and Director of PHOTU.</p>
                                     <p>Over the past three years I've been working hard to develop my skills within HTML, CSS and PHP to ensure the website is the best it can possibly be,
                                         I hope to continue this for as long as possible.</p>
-                                    <div class="kopa-social-links style2">
-                                        <ul class="clearfix">
-                                            <li><a target="_blank" href="www.facebook.com/officialphotu" class="fa fa-facebook"></a></li>
-                                            <li><a target="_blank" href="www.witter.com/officialphotu" class="fa fa-twitter"></a></li>
-                                        </ul>
-                                    </div>
                                 </div>
                             </div>
                             <!-- kopa-author -->
--- php/authors/matty-gale.php
+++ php/authors/matty-gale.php
@@ -3,11 +3,6 @@
                                 <div class="author-content clearfix">
                                     <h4 class="author-name">ABOUT THE AUTHOR - <a href="https://www.youtube.com/user/MattyGaleDickEd" target=_"blank">MATTY</a></h4>
                                     <p>Matty is a content producer for YouTube. <a target="_blank" href="https://www.youtube.com/user/MattyGaleDickEd">Click here to view his channel</a>.</p>
-                                    <div class="kopa-social-links style2">
-                                        <ul class="clearfix">
-                                            <li><a href="https://www.facebook.com/matty.gale.92" target="_blank" class="fa fa-facebook"></a></li>
-                                        </ul>
-                                    </div>
                                 </div>
                             </div>
                             <!-- kopa-author -->
