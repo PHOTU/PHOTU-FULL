9d28bfd674841f0beea1ba822d7dcea580e3f372
PHOTU <officialphotu@gmail.com>
2016-01-21 22:32:19 +0000

    Updated 404

--- 404.php
+++ 404.php
@@ -1,59 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>404! - PHOTU</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="You've reached a page that can't be found!">
-        <meta name="author" content="">
-        <link rel="stylesheet" type="text/css" href="/css/bootstrap.css" media="all" />
-        <link rel="stylesheet" type="text/css" href="/css/font-awesome.css" media="all" />
-        <link rel="stylesheet" type="text/css" href="/css/superfish.css" media="all" />
-        <link rel="stylesheet" type="text/css" href="/css/megafish.css" /> 
-        <link rel="stylesheet" type="text/css" href="/css/jquery.navgoco.css"/>
-        <link rel="stylesheet" type="text/css" href="/css/owl.carousel.css" media="all" />
-        <link rel="stylesheet" type="text/css" href="/css/owl.theme.css" media="all" />
-        <link rel="stylesheet" type="text/css" href="/css/woocommerce.css" media="all" />
-        <link rel="stylesheet" type="text/css" href="/style-blue.css">
-        
-        <script src="/js/modernizr.custom.js"></script>
+        <title>PHOTU - 404</title>
+        <meta name="description" content="Welcome to PHOTU - Entries and updates submitted by you.">
 
-        <!--[if lt IE 9]>
-            <link rel="stylesheet" href="css/ie.css" type="text/css" media="all" />
-        <![endif]-->
+<?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/loadinstyles.php' ); ?>
 
-        <!--[if IE 9]>
-            <link rel="stylesheet" href="css/ie9.css" type="text/css" media="all" />
-        <![endif]-->
-
-        <!-- -fav and touch icons -->
-        <link rel="shortcut icon" href="/img/favicon.ico">
-        <link rel="apple-touch-icon" href="/img/apple-touch-icon.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="/img/apple-touch-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="/img/apple-touch-icon-114x114.png">
-
-    </head>
-    
-<body class="">
+<body class="kopa-home-page">
 
     <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/header.php' ); ?>
-
-    <div class="kopa-breadcrumb">
-        <div class="wrapper clearfix">
-            <span itemtype="http://data-vocabulary.org/Breadcrumb" itemscope="">
-                <a itemprop="url" href="#">
-                    <span itemprop="title">Home</span>
-                </a>
-            </span>
-            &nbsp;|&nbsp;
-            <span itemtype="http://data-vocabulary.org/Breadcrumb" itemscope="">
-                <a itemprop="url" class="current-page">
-                    <span itemprop="title">404</span>
-                </a>
-            </span>
-        </div>
-    </div>
-    <!--/end .breadcrumb-->
 
     <div id="main-content" class="mb-20">
 
@@ -66,14 +20,14 @@
                 <div class="right-col">
                     <h1>Page not found...</h1>
                     <p>We're sorry, but we can't find the page you were looking for. It's probably something we've done wrong but now we know about it we'll try to fix it. In the meantime, try one of these options:</p>
-                    <ul class="arrow-list">                   
+                    <ul class="arrow-list">
                         <li>
                             <a href="/">Go to homepage</a>
                         </li>
                     </ul>
                 </div><!--right-col-->
             </section>
-        
+
         </div>
         <!-- wrapper -->
 
@@ -81,7 +35,7 @@
     <!-- main-content -->
 
     <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/footer.php' ); ?>
-    
+
 </body>
 
 </html>
