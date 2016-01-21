d7d19da3939352f9cb252c280be781fcf199abd8
PHOTU <officialphotu@gmail.com>
2016-01-21 21:40:43 +0000

    Mobile Changes

--- php/header.php
+++ php/header.php
@@ -68,15 +68,9 @@
 
 
 
-
         <!-- MAIN MOBILE MENU -->
 
-                    <!--<nav class="main-nav-mobile">
-                        <a class="pull">BROWSE<i class="fa fa-caret-down"></i></a>
-                        <ul class="main-menu-mobile">
-                            <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/menulist.php' ); ?>
-                        </ul>
-                    </nav>-->
+
 
         <!-- MAIN MOBILE MENU -->
 
---
+++ php/navmenucode.php
@@ -0,0 +1,6 @@
+<nav class="main-nav-mobile">
+    <a class="pull">BROWSE<i class="fa fa-caret-down"></i></a>
+    <ul class="main-menu-mobile">
+        <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/menulist.php' ); ?>
+    </ul>
+</nav>
