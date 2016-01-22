b01eb4b71b8f5c6a1f1fae00a8bf4bee98d5a3cc
PHOTU <officialphotu@gmail.com>
2016-01-22 13:54:42 +0000

    ..

--- index.php
+++ index.php
@@ -36,8 +36,8 @@
             <!-- Flexible Entries Area -->
                         <div class="widget-area-8">
 
-                            <div class="widget kopa-categories-widget red-title">
-                                <a href="/categories/technology/dev-blog/week-one.php">DEV BLOG - WEEK ONE</a>
+                            <div class="widget kopa-categories-widget">
+                                  <a.red-title href="/categories/technology/dev-blog/week-one.php">DEV BLOG - WEEK ONE</a.red-title>
                                 <div>
                                     <div>
 
