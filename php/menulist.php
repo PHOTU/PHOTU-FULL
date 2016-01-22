6b31c05178a9e28dd4b0328bd291cffa812a0e06
PHOTU <officialphotu@gmail.com>
2016-01-22 14:46:16 +0000

    Menu Stylin

--- php/menulist.php
+++ php/menulist.php
@@ -1,6 +1,15 @@
                             <li class="current-menu-item">
                                 <a href="/">Home</a>
                             </li>
+                            <li class="">
+                                <a href="">Categories</a>
+                                <ul class="sub-menu" style="display: none;">
+                                    <li><a href="/categories/technology">Technology</a></li>
+                                    <li><a href="/categories/music">Music</a></li>
+                                    <li><a href="/categories/general">General</a></li>
+                                    <li><a href="/categories/reviews">Reviews</a></li>
+                                </ul>
+                            </li>
                             <li class="menu-item">
                                 <a href="/categories/technology">Technology</a>
                             </li>
@@ -17,6 +26,7 @@
                             <li class="menu-item">
                                 <a href="/contact">Get In Touch</a>
                             </li>
+
 
 
 
