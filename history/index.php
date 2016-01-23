7819b252a1cd8b972a4959e46dcb0b87ef0b4a70
PHOTU <officialphotu@gmail.com>
2016-01-23 18:34:01 +0000

    Adding History

---
+++ history/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+        <title>PHOTU</title>
+        <meta name="description" content="Welcome to PHOTU - Entries and updates submitted by you.">
+
+<?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/loadinstyles.php' ); ?>
+
+<body class="kopa-home-page">
+
+    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/header.php' ); ?>
+
+    <div id="main-content" class="mb-20">
+
+        <div class="wrapper">
+
+            <div class="widget-area-5">
+
+                <div class="widget kopa-masonry-widget" align="centre">
+                    <ul class="">
+
+
+                        <li class="1">
+                            <article class="">
+                                <div class="">
+                                    <img src="/images/history/cover.jpg" alt="">
+                                </div>
+                            </article>
+                        </li>
+
+                    </ul>
+                </div>
+
+            </div>
+
+
+            <!-- Flexible Entries Area -->
+                        <div class="widget-area-8">
+
+                            <div class="widget kopa-categories-widget">
+                                  <red-title><a class="red-title">OUR HISTORY</a></red-title>
+                                <div>
+                                    <div>
+
+
+            <!-- Add content here -->
+
+            <p>Good evening guys, it's been a long night of coding, I'll update this tomorrow</p>
+
+            <!-- End content here -->
+
+
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+            </div>
+
+            </div>
+            <!-- row -->
+
+
+                <!-- kopa-main-col -->
+
+
+                </div>
+                <!-- sidebar -->
+
+            </div>
+            <!-- row -->
+
+        </div>
+        <!-- wrapper -->
+
+    </div>
+    <!-- main-content -->
+
+    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/footer.php' ); ?>
+
+</body>
+
+</html>
