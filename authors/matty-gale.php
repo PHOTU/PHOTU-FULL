e37b6343aa826fea4bf0ab5681fe0ad52ad053a9
PHOTU <officialphotu@gmail.com>
2016-01-23 21:50:26 +0000

    Fixed Mobile Nav Padding & Typo

--- authors/matty-gale.php
+++ authors/matty-gale.php
@@ -7,7 +7,7 @@
                 </div>
                 <div class="entry-content">
                     <h4 class="entry-title" itemscope="" itemtype="http://schema.org/Event"><itemprop="name">MATTY GALE</h4>
-                    <p>I'm Matty Gale, I became part of PHOTU in late 2016 / early 2016. I produce content for my personal Youtube channel, but I'm here to get involved and collaborate my content to produce the best videos possible.</p>
+                    <p>I'm Matty Gale, I became part of PHOTU in late 2015. I produce content for my personal Youtube channel, but I'm here to get involved and collaborate my content to produce the best videos possible.</p>
                 </div>
             </div>
         </article>
