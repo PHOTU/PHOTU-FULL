1a1f712ebac311f96a27b22a007bc23e693c0182
PHOTU <officialphotu@gmail.com>
2016-01-23 16:00:31 +0000

    Add Matty Gale

---
+++ authors/matty-gale.php
@@ -0,0 +1,14 @@
+<li class="ms-item6">
+        <article class="entry-item video-post">
+            <div class="pd-20">
+                <div class="entry-thumb style1">
+                    <img src="/images/authors/matty-gale.jpg" alt="">
+                    <h5><a>Producer</a></h5>
+                </div>
+                <div class="entry-content">
+                    <h4 class="entry-title" itemscope="" itemtype="http://schema.org/Event"><itemprop="name">MATTY GALE</h4>
+                    <p>I'm Matty Gale, video producer at PHOTU.</p>
+                </div>
+            </div>
+        </article>
+    </li>
