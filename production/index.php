d3e8a73f38aaab008d587198001778f5739947ce
PHOTU <officialphotu@gmail.com>
2016-01-24 15:58:52 +0000

    Changes

---
+++ production/index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <title>PHOTU - Production Schedule</title>
+    <meta content="Welcome to PHOTU - Entries and updates submitted by you."
+    name="description">
+    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/loadinstyles.php' ); ?>
+</head>
+
+<body class="kopa-home-page">
+    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/header.php' ); ?>
+
+    <div class="mb-20" id="main-content">
+        <div class="wrapper">
+        <div class="row style1">
+        <div class="widget-area-5">
+        <div class="col-md-6 col-sm-6 col-xs-6">
+        <red-title><a class="red-title">Tuesday</a></red-title>
+
+        Rockets sprang out of the hills before them so soon
+        as they began to move, and warned the waiting
+        batteries about Ditton and Esher. At the same time
+        four of their fighting machines, similarly armed
+        with tubes, crossed the river, and two of them. A
+        dozen rockets sprang out of the hills before them
+        so soon as they began to move, and warned the
+        waiting batteries about Ditton and Esher. At the
+        same time four of their fighting machines,
+        similarly armed with tubes, crossed the river, and
+        two of them. A dozen rockets sprang out of the
+        hills before them so soon as they began to move,
+        and warned the waiting batteries about Ditton and
+        Esher. At the same time four of their fighting
+        machines, similarly armed with tubes, crossed the
+        river, and two of them.
+
+        </div>
+
+
+        <div class="col-md-6 col-sm-6 col-xs-6">
+        <red-title><a class="red-title">Friday</a></red-title>
+
+        Rockets sprang out of the hills before them so soon
+        as they began to move, and warned the waiting
+        batteries about Ditton and Esher. At the same time
+        four of their fighting machines, similarly armed
+        with tubes, crossed the river, and two of them. A
+        dozen rockets sprang out of the hills before them
+        so soon as they began to move, and warned the
+        waiting batteries about Ditton and Esher. At the
+        same time four of their fighting machines,
+        similarly armed with tubes, crossed the river, and
+        two of them. A dozen rockets sprang out of the
+        hills before them so soon as they began to move,
+        and warned the waiting batteries about Ditton and
+        Esher. At the same time four of their fighting
+        machines, similarly armed with tubes, crossed the
+        river, and two of them.
+
+        </div>
+
+
+        <div class="col-md-6 col-sm-6 col-xs-6">
+        <red-title><a class="red-title">Saturday</a></red-title>
+
+        Rockets sprang out of the hills before them so soon
+        as they began to move, and warned the waiting
+        batteries about Ditton and Esher. At the same time
+        four of their fighting machines, similarly armed
+        with tubes, crossed the river, and two of them. A
+        dozen rockets sprang out of the hills before them
+        so soon as they began to move, and warned the
+        waiting batteries about Ditton and Esher. At the
+        same time four of their fighting machines,
+        similarly armed with tubes, crossed the river, and
+        two of them. A dozen rockets sprang out of the
+        hills before them so soon as they began to move,
+        and warned the waiting batteries about Ditton and
+        Esher. At the same time four of their fighting
+        machines, similarly armed with tubes, crossed the
+        river, and two of them.
+
+        </div>
+
+
+
+
+
+
+
+
+
+          </div>
+        </div>
+      </div>
+
+
+            <div class="widget-area-8">
+                <div class="widget kopa-categories-widget">
+                    <div>
+                        <div>
+                            <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/authors/matty-gale.php' ); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/footer.php' ); ?>
+</body>
+</html>
