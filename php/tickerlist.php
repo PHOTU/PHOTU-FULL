2cdd74417bff7ca32e1f1a1c3dea5fb8bfbb6060
PHOTU <officialphotu@gmail.com>
2016-01-24 14:43:49 +0000

    Test Content

--- php/tickerlist.php
+++ php/tickerlist.php
@@ -2,5 +2,7 @@
                         <h6 class="ticker-title">Latest News</h6>
                         <ul id="js-news" class="news-white-text">
                             <li><a href="/categories/music/">Check out our new Discover Weekly to find some refreshing music to listen to!</li>
+                            <li><a href="/categories/technology/">Wanna check up on what the team have beeb up to? Read the Weekly Dev Blogs!</li>
+                            <li>A big thank you to everyone for their continued support on our project</li>
                         </ul>
                     </div>
---
+++ tuesday-tips/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+        <title>PHOTU - Tuesday Tips</title>
+        <meta name="description" content="Welcome to PHOTU - Entries and updates submitted by you.">
+
+<?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/loadinstyles.php' ); ?>
+
+<body class="kopa-home-page">
+
+    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/header.php' ); ?>
+
+    <div id="main-content" class="mb-20">
+
+        <div class="wrapper">
+
+            <div class="widget-area-5">
+
+                <div class="widget kopa-masonry-widget" align="centre">
+                    <ul class="">
+
+
+                        <li class="1">
+                            <article class="">
+                                <div class="">
+                                    <img src="/images/history/cover.jpg" alt="">
+                                </div>
+                            </article>
+                        </li>
+
+                    </ul>
+                </div>
+
+            </div>
+
+
+            <!-- Flexible Entries Area -->
+                        <div class="widget-area-8">
+
+                            <div class="widget kopa-categories-widget">
+                                  <red-title><a class="red-title">Tuesday Tips</a></red-title>
+                                <div>
+                                    <div>
+
+
+            <!-- Add content here -->
+
+            <p>Good evening guys, it's been a long night of coding, I'll update this tomorrow</p>
+
+            <!-- End content here -->
+
+
+
+
+            <h6 class="c-title">Previous Videos..</h6>
+                      <div class="row style1">
+                          <div class="col-md-4 col-sm-4 col-xs-4">
+                              <img src="/images/history/cover.jpg" alt="">
+                          </div>
+                          <div class="col-md-4 col-sm-4 col-xs-4">
+                              <img src="/images/history/cover.jpg" alt="">
+                          </div>
+                          <div class="col-md-4 col-sm-4 col-xs-4">
+                              <img src="/images/history/cover.jpg" alt="">
+                          </div>
+                      </div>
+
+
+
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+            </div>
+
+            </div>
+            <!-- row -->
+
+
+                <!-- kopa-main-col -->
+
+
+                </div>
+                <!-- sidebar -->
+
+            </div>
+            <!-- row -->
+
+        </div>
+        <!-- wrapper -->
+
+    </div>
+    <!-- main-content -->
+
+    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/php/footer.php' ); ?>
+
+</body>
+
+</html>
